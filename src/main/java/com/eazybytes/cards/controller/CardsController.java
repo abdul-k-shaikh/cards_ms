39f9178c334144d8d505dcf5928b9204aad566e8
theabdul97@gmail.com <theabdul97@gmail.com>
2024-08-26 17:57:23 +0530

    SpringBootProfileChangesInsideCardsMs

--- src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -1,5 +1,8 @@
 package com.eazybytes.cards.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.cards.constants.CardsConstants;
+import com.eazybytes.cards.dto.CardsContactInfoDto;
 import com.eazybytes.cards.dto.CardsDto;
 import com.eazybytes.cards.dto.ErrorResponseDto;
 import com.eazybytes.cards.dto.ResponseDto;
@@ -26,7 +30,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 
 /**
  * @author Abdul K Shaikh on 15Aug2024
@@ -35,11 +38,23 @@ import lombok.AllArgsConstructor;
 @Tag(name = "CRUD REST APIs for Cards in EazyBank", description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details")
 @RestController
 @RequestMapping(path = "/api")
-@AllArgsConstructor
 @Validated
 public class CardsController {
 
 	private ICardsService iCardsService;
+
+	public CardsController(ICardsService iCardsService) {
+		this.iCardsService = iCardsService;
+	}
+
+	@Value("${build.version}")
+	private String buildVersion;
+
+	@Autowired
+	private Environment environment;
+
+	@Autowired
+	private CardsContactInfoDto cardsContactInfoDto;
 
 	@Operation(summary = "Create Card REST API", description = "REST API to create new Card inside EazyBank")
 	@ApiResponses({ @ApiResponse(responseCode = "201", description = "HTTP Status CREATED"),
@@ -93,6 +108,30 @@ public class CardsController {
 			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
 					.body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_DELETE));
 		}
+	}
+
+	@Operation(summary = "Get Build information", description = "Get Build information that is deployed into cards microservice")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+			@ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))) })
+	@GetMapping("/build-info")
+	public ResponseEntity<String> getBuildInfo() {
+		return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+	}
+
+	@Operation(summary = "Get Java version", description = "Get Java versions details that is installed into cards microservice")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+			@ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))) })
+	@GetMapping("/java-version")
+	public ResponseEntity<String> getJavaVersion() {
+		return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+	}
+
+	@Operation(summary = "Get Contact Info", description = "Contact Info details that can be reached out in case of any issues")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+			@ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))) })
+	@GetMapping("/contact-info")
+	public ResponseEntity<CardsContactInfoDto> getContactInfo() {
+		return ResponseEntity.status(HttpStatus.OK).body(cardsContactInfoDto);
 	}
 
 }
